8286733978b390a56f9c3bfbf4ab7bf20ea95e2e
Thiagosjcx <thisaint07@gmail.com>
2026-04-10 19:20:32 -0300

    migration

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Api_Compra_e_Venda.Models;
+
+namespace Api_Compra_e_Venda.Context {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Ativo> Ativos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseNpgsql("Host=localhost;Database=compra_venda;Username=postgres;Password=yourpassword");
+            }
+        }
+    }
+}
---
+++ Models/Ativo.cs
@@ -0,0 +1,8 @@
+namespace Api_Compra_e_Venda.Models;
+
+public class Ativo
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public decimal Preco { get; set; }        
+}
