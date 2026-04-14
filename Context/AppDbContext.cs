6f6991ed4ca21279f78bf4efdbf39fbaebf27469
Thiagosjcx <thisaint07@gmail.com>
2026-04-14 18:42:42 -0300

    stock

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Api_Compra_e_Venda.Models;
 
-namespace Api_Compra_e_Venda.Context {
-    public class AppDbContext : DbContext {
+namespace Api_Compra_e_Venda.Context
+{
+    public class AppDbContext : DbContext
+    {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Ativo> Ativos { get; set; }
+        public DbSet<Stock> Stocks { get; set; } 
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            if (!optionsBuilder.IsConfigured) {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
                 optionsBuilder.UseNpgsql("Host=localhost;Database=compra_venda;Username=postgres;Password=yourpassword");
             }
         }
--- obj/Release/net10.0/Api-Compra-e-Venda.AssemblyInfo.cs
+++ obj/Release/net10.0/Api-Compra-e-Venda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api-Compra-e-Venda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22bac34439d76acae96b8c27ffb270b2021489b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api-Compra-e-Venda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api-Compra-e-Venda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
